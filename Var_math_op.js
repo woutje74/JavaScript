3fde4efb07ec66d766ed3957abf51ba37016dab6
woutje74 <60627581+woutje74@users.noreply.github.com>
2020-02-13 11:41:01 +0100

    first try

---
+++ Var_math_op.js
@@ -0,0 +1,24 @@
+//Mathmatical assignment operators
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+//Mathmatical increment operators
+let w = 4;
+let y = 10;
+w++;
+console.log(w);
+y--;
+console.log(y);
+
---
+++ myName.js
@@ -0,0 +1,4 @@
+//String interpolation
+let myName = 'Wouter';
+let myCity = 'Rotterdam';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
